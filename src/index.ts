5565cef305f4ddabc2ae9ce6b12a712bc6c5d55e
Guillermo Segura <correo@seguraguillermo.net>
2024-03-20 23:48:39 +0000

    feat: added new credential routes to index file

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-require('reflect-metadata');
 // https://stackoverflow.com/questions/27676884/explicitly-specifying-types-for-express-application-request-response
 import express = require('express');
 import http = require('http');
@@ -6,25 +5,24 @@ import http = require('http');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const credentialRoutes = require('./routes/api/credentialRoutes');
 
 dotenv.config();
 
 const app: express.Application = express();
 app.use(bodyParser.json());
+app.use(credentialRoutes);
 app.use(cors());
 
-const port = process.env.PORT;
-const url = process.env.APP_URL;
+const PORT = process.env.PORT;
+const URL = process.env.APP_URL;
+
 let server: http.Server | null = null;
 
-app.use('/', (req: express.Request, res: express.Response) => {
-  return res.status(200).json({ message: 'Hello World!' });
+app.use('/', credentialRoutes);
+
+app.listen(PORT, async () => {
+  console.log(`[SERVER]: Listening at ${URL}:${PORT}`);
 });
 
-server = app.listen(port, async () => {
-  console.log(`[server]: Server is running at ${url}:${port}`);
-});
-
-export { server };
-
-export default app;
+module.exports = app;
