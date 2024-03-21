4fe0f1f0fad9bee1f3283ea98580c9d3e3b08fc9
Guillermo Segura <correo@seguraguillermo.net>
2024-03-21 00:56:21 +0000

    feat: added new auth middleware to credential post route

--- src/routes/api/credentialRoutes.ts
+++ src/routes/api/credentialRoutes.ts
@@ -1,10 +1,11 @@
 import express = require('express');
 
 import { issueCredential } from '../../controllers/credentialController';
+const automaticAuth = require('../../middlewares/automaticAuth');
 
 const router = express.Router();
 
 // curl -X POST -H "Content-Type: application/json" -d '{"givenName": "", "email": "", "countryOfResidence": "", "dateOfBirth": "", "photo": ""}' http://localhost:3000/api/issue-credential
-router.post('/api/issue-credential', issueCredential);
+router.post('/api/issue-credential', automaticAuth, issueCredential);
 
 module.exports = router;
