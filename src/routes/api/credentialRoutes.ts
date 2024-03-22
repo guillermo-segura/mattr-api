6e78ee6d61148a342ba586b36160c1c2577f34d6
Guillermo Segura <correo@seguraguillermo.net>
2024-03-22 16:40:09 +0000

    feat: added validation to issue credential endpoint

--- src/routes/api/credentialRoutes.ts
+++ src/routes/api/credentialRoutes.ts
@@ -1,11 +1,19 @@
 import express = require('express');
+const { body } = require('express-validator');
 
 import { issueCredential } from '../../controllers/credentialController';
-const automaticAuth = require('../../middlewares/automaticAuth');
 
 const router = express.Router();
 
-// curl -X POST -H "Content-Type: application/json" -d '{"givenName": "", "email": "", "countryOfResidence": "", "dateOfBirth": "", "photo": ""}' http://localhost:3000/api/issue-credential
-router.post('/api/issue-credential', automaticAuth, issueCredential);
+const validations = [
+  body("givenName").exists().isString().notEmpty(),
+  body("email").exists().isString().notEmpty(),
+  body("countryOfResidence").exists().isString().notEmpty(),
+  body("photo").exists().isString(),
+  body("walletDid").exists().isString().notEmpty(),
+];
+
+// curl -X POST -H "Content-Type: application/json" -d '{"givenName": "John Doe", "email": "john@doe.com", "countryOfResidence": "New Zealand", "dateOfBirth": "1990-01-01", "photo": "", "walletDid": "did:key:1234"}' http://localhost:3001/api/issue-credential
+router.post('/api/issue-credential', validations, issueCredential);
 
 module.exports = router;
