ce32df0aad5c80a2919d80772af1c289858b1b3f
Guillermo Segura <correo@seguraguillermo.net>
2024-03-22 16:38:44 +0000

    feat: created mattrvii api file with interceptor

---
+++ src/api/mattrVII.ts
@@ -0,0 +1,40 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: process.env.BASE_URL,
+});
+
+const createApiAuthToken = async (clientId?: string, clientSecret?: string, clientAudience?: string) => {
+  const url = `${process.env.AUTH_URL}/oauth/token`;
+  const headers = { 'Content-Type': 'application/json' };
+  const body = {
+    client_id: clientId,
+    client_secret: clientSecret,
+    audience: clientAudience,
+    grant_type: 'client_credentials'
+  };
+
+  try {
+    const response = await axios.post(url, body, { headers });
+    return response.data;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+instance.interceptors.request.use(
+  async (config) => {
+    const response = await createApiAuthToken(
+      process.env.AUTH_CLIENT_ID,
+      process.env.AUTH_CLIENT_SECRET,
+      process.env.AUTH_AUDIENCE,
+    );
+    if (response?.access_token) {
+      config.headers.Authorization = `Bearer ${response?.access_token}`;
+    }
+    return config;
+  },
+  (err) => { return Promise.reject(err); },
+);
+
+export default instance;
