bd933cfe0a82f62d68ab8defcc11eafed3740033
Guillermo Segura <correo@seguraguillermo.net>
2024-03-22 19:29:30 +0000

    feat: updated credential controller

--- src/controllers/credentialController.ts
+++ src/controllers/credentialController.ts
@@ -4,18 +4,17 @@ const issueCredentialUseCase = require('../useCases/issueCredentialUseCase');
 
 export const issueCredential = async (req: express.Request, res: express.Response) => {
   const { givenName, email, countryOfResidence, dateOfBirth, photo, walletDid } = req.body;
-
-  console.log('[Controller] Data received');
-
-  // @TODO VALIDATE DATA
-  console.log('[Controller] Data validated');
-
   try {
-    const credential = issueCredentialUseCase({ givenName, email, countryOfResidence, dateOfBirth, photo, walletDid });
-    res
-      .status(201)
-      .json({ data: credential });
+    if (issueCredentialUseCase(walletDid, { givenName, email, countryOfResidence, dateOfBirth, photo })) {
+      res
+        .status(201)
+        .json({ message: 'Credential Issued' });
+    } else {
+      res
+        .status(400)
+        .json({ message: 'Bad Request' });
+    }
   } catch (err) {
-    res.status(500).json({ message: 'Server error' });
+    res.status(500).json({ message: 'Server Error' });
   }  
 };
